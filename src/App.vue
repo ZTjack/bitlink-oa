a8a9e5e942d72e1684503703e543ab6d748f6836
kmmoonlight <kmmoonlight@outlook.com>
2022-09-23 15:47:26 +0800

    add grafana shortcuts choose

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
 </template>
 
 <style lang="scss">
+body {
+  margin: 0;
+}
+
 #app {
+  height: 100%;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,44 +1,82 @@
 <template>
   <div class="home">
-    <el-menu @select="handleSelect">
-      <el-submenu
-        v-for="config in configs"
-        :key="config.name"
-        :index="config.name"
-      >
-        <template slot="title">
-          <span> {{ config.name }}</span>
-          <el-menu-item
-            v-for="graph in config.graphs"
-            :key="graph.graph"
-            :index="graph.graph"
-          >
-            {{ graph.graph }}
-          </el-menu-item>
-        </template>
-      </el-submenu>
-    </el-menu>
+    <div class="sidebar-area">
+      <el-menu @select="handleSelect">
+        <el-submenu
+          v-for="config in configs"
+          :key="config.name"
+          :index="config.name"
+        >
+          <template #title>
+            <span> {{ config.name }}</span>
+          </template>
+          <el-menu-item-group v-for="graph in config.graphs" :key="graph.graph">
+            <template #title>{{ graph.graph }}</template>
+            <el-menu-item
+              v-for="shortItem in graph.shortcuts"
+              :key="shortItem.params"
+              :index="`${shortItem.params}---${graph.graph}`"
+            >
+              {{ shortItem.label }}
+            </el-menu-item>
+          </el-menu-item-group>
+        </el-submenu>
+      </el-menu>
+    </div>
+    <el-divider direction="vertical" class="divider-vertical-style" />
+    <div class="operation-area">
+      <div class="url-label-area">
+        <div class="label-style">QueryURL:</div>
+        <el-input v-model="mainPath" class="path-input-style" size="mini" />
+        <el-button
+          type="primary"
+          class="search-btn-style"
+          size="mini"
+          @click="searchUrl"
+        >
+          Search
+        </el-button>
+      </div>
+
+      <div class="form-area">
+        <div class="label-style">Graph:</div>
+        <el-input v-model="graph" size="mini" />
+        <div class="label-style">Params:</div>
+        <el-input v-model="formParams" size="mini" />
+        <div class="label-style">Time:</div>
+        <el-input v-model="timeRange" size="mini" />
+      </div>
+
+      <el-divider class="horizontal-divider-style" />
+
+      <iframe :src="realUsePath" class="grafana-area" />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
 @Component({
-  components: {
-    HelloWorld,
-  },
+  name: "HomeView",
 })
 export default class HomeView extends Vue {
   private graphPrefix = "//grafana.qbtrade.org/dashboard/db/";
 
-  private form = {
-    graph: "stra-dionysus-template",
-    params: "var-stra=strategy%2Fzop-mm1-btc-usd&refresh=1m",
-    timeRange: "24h",
-  };
+  //grafana的主路径
+  private mainPath = "";
 
-  // `${this.graphPrefix}/${graph}?${form.params}&from=now-${form.timeRange}&to=now`
+  //graph值
+  private graph = "";
+
+  //参数值
+  private formParams = "";
+
+  //时间值
+  private timeRange = "24h";
+
+  //iframe实际使用的URL
+  private realUsePath = "";
 
   private configs = [
     {
@@ -74,9 +112,75 @@ export default class HomeView extends Vue {
     },
   ];
 
-  private handleSelect(key: string, selects: string[]) {
-    console.log("key", key);
-    console.log("selects", selects);
+  //点击选择某一项
+  private handleSelect(key: string) {
+    const [params, graph] = key.split("---");
+    this.graph = graph;
+    this.formParams = params;
+    this.changeUrl();
+  }
+
+  private changeUrl() {
+    this.mainPath = `${this.graphPrefix}/${this.graph}?${this.formParams}&from=now-${this.timeRange}&to=now`;
+  }
+
+  private searchUrl() {
+    this.realUsePath = this.mainPath;
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  display: flex;
+}
+
+.sidebar-area {
+  width: 10%;
+}
+
+.operation-area {
+  margin-top: 10px;
+  width: 100%;
+
+  .url-label-area {
+    display: flex;
+    align-items: center;
+  }
+
+  .path-input-style {
+    width: 100%;
+    margin-right: 10px;
+  }
+
+  .search-btn-style {
+    margin-right: 10px;
+  }
+}
+
+.label-style {
+  margin: 0 10px;
+  height: 40px;
+  line-height: 40px;
+}
+
+.divider-vertical-style {
+  height: 100vh;
+}
+
+.form-area {
+  margin-top: 10px;
+  display: flex;
+  margin-right: 10px;
+  align-items: center;
+}
+
+.grafana-area {
+  width: 100%;
+  height: 100%;
+}
+
+.horizontal-divider-style {
+  margin: 0;
+}
+</style>
