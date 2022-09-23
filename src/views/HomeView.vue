8d5b7f5895128348100a068ebd63857de24c14f5
jack <jack@1token.trade>
2022-09-23 10:45:07 +0800

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,82 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <el-menu @select="handleSelect">
+      <el-submenu
+        v-for="config in configs"
+        :key="config.name"
+        :index="config.name"
+      >
+        <template slot="title">
+          <span> {{ config.name }}</span>
+          <el-menu-item
+            v-for="graph in config.graphs"
+            :key="graph.graph"
+            :index="graph.graph"
+          >
+            {{ graph.graph }}
+          </el-menu-item>
+        </template>
+      </el-submenu>
+    </el-menu>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
-
 @Component({
   components: {
     HelloWorld,
   },
 })
-export default class HomeView extends Vue {}
+export default class HomeView extends Vue {
+  private graphPrefix = "//grafana.qbtrade.org/dashboard/db/";
+
+  private form = {
+    graph: "stra-dionysus-template",
+    params: "var-stra=strategy%2Fzop-mm1-btc-usd&refresh=1m",
+    timeRange: "24h",
+  };
+
+  // `${this.graphPrefix}/${graph}?${form.params}&from=now-${form.timeRange}&to=now`
+
+  private configs = [
+    {
+      name: "lwq",
+      graphs: [
+        {
+          graph: "stra-dionysus-template",
+          shortcuts: [
+            {
+              params: "var-stra=strategy%2Fzop-mm1-btc-usd&refresh=1m",
+              label: "BTC-USD",
+            },
+            {
+              params: "var-stra=strategy%2Fzop-mm1-eth-usd&refresh=1m",
+              label: "ETH-USD",
+            },
+          ],
+        },
+        {
+          graph: "stra-dionysus-template2",
+          shortcuts: [
+            {
+              params: "var-stra=strategy%2Fzop-mm1-btc-usd&refresh=1m",
+              label: "BTC-USD2",
+            },
+            {
+              params: "var-stra=strategy%2Fzop-mm1-eth-usd&refresh=1m",
+              label: "ETH-USD2",
+            },
+          ],
+        },
+      ],
+    },
+  ];
+
+  private handleSelect(key: string, selects: string[]) {
+    console.log("key", key);
+    console.log("selects", selects);
+  }
+}
 </script>
